3da2b5db598ee2d388394d6ae493b1b199d8d1d7
Elena Aksenova <lenta4ka2010@mail.ru>
2025-03-07 14:57:49 +0300

    context

--- notes/tests/test_content.py
+++ notes/tests/test_content.py
@@ -16,56 +16,33 @@ class TestHomePage(TestCase):
     @classmethod
     def setUpTestData(cls):
         cls.author = User.objects.create(username='Lena')
-        cls.notes = Note.objects.create( author=cls.author,
+        cls.no_author = User.objects.create(username='Gena')
+        cls.note = Note.objects.create( author=cls.author,
             text='Текст комментария',
             title='Заголовок',
             slug='Slug')
         cls.list_url = reverse('notes:list')
+        cls.add_url = reverse ('notes:add')
+        cls.edit_url = reverse('notes:edit', args=(cls.note.slug,))
+        #cls.detail_url = reverse('notes:detail', args=(cls.note.slug,))
+
     def test_note_in_list(self):
         self.client.force_login(self.author)
         response = self.client.get(self.list_url)
-        #print(self.notes in response.context['object_list'])
+        #print(self.note in response.context['object_list'])
         # Проверяем, что сам объект новости находится в словаре контекста
-        self.assertIn(self.notes, response.context['object_list'])
-'''class TestHomePage(TestCase):
+        self.assertIn(self.note, response.context['object_list'])
 
-    HOME_URL = reverse('notes:home')
-    
-    @classmethod
-    def setUpTestData(cls):
-        cls.author = User.objects.create(username='Комментатор')
-        today = datetime.today()
-        cls.notes = Note.objects.create(
-            author=cls.author,
-            text='Текст комментария',
-            title='Заголовок',
-            slug='Slug')
-        # Сохраняем в переменную адрес страницы с новостью:
-        cls.list_url = reverse('notes:list')
-        print(cls.list.url)
-        #cls.author = User.objects.create(username='Комментатор')
-all_notes = []
-        for index in range(settings.NEWS_COUNT_ON_HOME_PAGE + 1):
-            notes = Note(title=f'Новость {index}', text='Просто текст.', date=today - timedelta(days=index))
-            for index in range(settings.NEWS_COUNT_ON_HOME_PAGE + 1):
-                all_notes.append(notes)
-        Note.objects.bulk_create(all_notes) 
-
-    def test_note_in_list_notes(self):
-        #self.client.force_login(self.author)
-        #response = self.client.get(self.HOME_URL)
-        #self.assertIn('notes', response.context)
-        #print(response)
-        # Проверяем, что объект новости находится в словаре контекста
-        # под ожидаемым именем - названием модели.
-        #self.assertIn('notes', response.context['notes_list'])
-        # Загружаем главную страницу.
+    def test_no_list_for_no_author(self):
+        self.client.force_login(self.no_author)
         response = self.client.get(self.list_url)
-        self.assertIn('notes', response.context)
-        # Код ответа не проверяем, его уже проверили в тестах маршрутов.
-        # Получаем список объектов из словаря контекста.
-        #object_list = response.context['object_list']
-       # self.assertIn('note', response.context['object_list'])  
-        # Определяем количество записей в списке.
-        # Проверяем, что на странице именно 10 новостей.'''
+        self.assertNotIn(self.note, response.context['object_list'])
         
+    def test_form_in_add_and_edit(self):
+        #self.add_url = reverse ('notes:add',args=(self.note.slug,))
+        urls_form=((self.edit_url, 'form'),(self.add_url,'form'))
+        self.client.force_login(self.author)
+        for url, form in urls_form:
+
+            response = self.client.get(url)
+            self.assertIn(form, response.context)
